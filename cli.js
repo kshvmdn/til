bd2c6e82572da48369aea30400ddae0d0c2b3f45
Kashav Madan <kshvmdn@gmail.com>
2016-03-10 02:01:04 -0500

    Parse argv for cli options

--- cli.js
+++ cli.js
@@ -3,3 +3,59 @@
 const til = require('./src/');
 const minimist = require('minimist');
 const version = require('./package.json').version;
+const defaults = {
+  boolean: [
+    'help',
+    'version',
+    'open'
+  ],
+  alias: {
+    h: 'help',
+    v: 'version',
+    p: 'posts',
+    o: 'open'
+  },
+  default: {
+    posts: 1,
+    open: false
+  }
+};
+
+const help = `
+  Usage: til [OPTIONS]
+    Open random TILs from /r/todayilearned.
+
+  Example:
+    $ til --posts 5 --open
+
+  Options:
+    -h --help         Display this help dialog
+    -v --version      Display current version
+    -p --posts        The number of TILs to display (defaults to 1)
+    -o --open         Whether or not to open the links for the TILs (defaults to false)
+`;
+
+const run = argv => til(argv);
+
+exports.exitCode = 0;
+
+exports.stdout = process.stdout;
+exports.stderr = process.stderr;
+
+exports.parse = options => minimist(options, defaults);
+
+exports.run = argv => {
+  exports.exitCode = 0;
+
+  if (argv.help) {
+    exports.stderr.write(help);
+    return;
+  }
+
+  if (argv.version) {
+    exports.stderr.write(`til v${version}\n`);
+    return;
+  }
+
+  run(argv);
+};
