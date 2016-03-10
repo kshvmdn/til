6aa0089beb3138965c1fb864324305fb6b1ddec6
Kashav Madan <kshvmdn@gmail.com>
2016-03-10 02:04:32 -0500

    Add fetch, filter for submissions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const fetchOptions = require('./options');
+const utils = require('./utils')();
+
+const fetchPosts = () => {
+  return utils.fetch(fetchOptions);
+};
+
+const filterPosts = posts => {
+  return utils.filter(posts);
+};
+
+const run = options => {
+  return fetchPosts()
+    .then(response => {
+      return filterPosts(response.body);
+    })
+    .then(response => {
+    })
+};
+module.exports = options => {
+  run(options);
+};
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const colors = require('colors');
+const got = require('got');
+const _ = require('underscore');
+const open = require('opn');
+
+module.exports = () => {
+  return {
+    fetch: opt => got('https://www.reddit.com/r/todayilearned.json?limit=5', opt),
+    filter: posts => {
+      // remove stickied posts (i.e. modposts (i.e. non-TILs))
+      const p = posts.data.children;
+      return _.filter(p, post => {
+        return !post.data.stickied;
+      });
+    },
+  };
+};
